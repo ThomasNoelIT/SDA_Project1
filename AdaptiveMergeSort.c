51228768bf309f66201dc990f087670d6a3ba5fa
ADRJ23 <125129383+ADRJ23@users.noreply.github.com>
2023-03-22 20:42:24 +0100

    Add files via upload

---
+++ AdaptiveMergeSort.c
@@ -0,0 +1,21 @@
+/* ========================================================================= *
+ * AdaptiveMergeSort
+ * Implementation of the Adaptive Merge Sort algorithm.
+ * ========================================================================= */
+
+#include "Sort.h"
+#include "Array.h"
+
+int findRun(int* array, size_t start, size_t end, size_t minSize);
+
+int findRun(int* array, size_t start, size_t end, size_t minSize) {
+
+    // to be completed
+    return start;
+}
+
+void sort(int *array, size_t length) {
+
+    // to be completed
+    return;
+}
